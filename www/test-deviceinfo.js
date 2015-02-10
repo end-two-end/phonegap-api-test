2ef03ee67f409d30b22b04f46ed4c179ac1929a0
Dewald Batt <dewald.batt@end2end.com>
2015-02-10 14:03:15 +0200

    delete old location js files

--- www/test-deviceinfo.js
+++
@@ -1,7 +0,0 @@
-var deviceInfo = function() {
-	document.getElementById("cordova").innerHTML = device.cordova;
-	document.getElementById("model").innerHTML = device.model;
-	document.getElementById("platform").innerHTML = device.platform;
-	document.getElementById("uuid").innerHTML = device.uuid;
-	document.getElementById("version").innerHTML = device.version;
-}
