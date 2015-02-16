de23df045138b8d66ab47434008187c77672f7c1
Dewaldtdj <dewaldt.dejager@end2end.com>
2015-02-16 14:17:07 +0200

    Updated authentication example

--- www/js/test-oauth.js
+++ www/js/test-oauth.js
@@ -1,13 +1,14 @@
 function authenticate(callback) {
     OAuth.initialize('Aqmf85hhmWlPjjypuuY2hk44A7o');
 
-    alert('Button Clicked');
     OAuth.popup('facebook')
 	.done(function(result) {
-	    result.get('/me')
+	    result.me()
 	    .done(function (response) {
-		//this will display "John Doe" in the console
-		alert('My name is: ' + response.name);
+		
+		document.getElementById("firstname").innerHTML = response.firstname;
+		document.getElementById("lastname").innerHTML = response.lastname;
+
 	    })
 	    .fail(function (err) {
 		//handle error with err
